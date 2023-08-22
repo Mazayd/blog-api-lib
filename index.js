e21bac2349bec009e5b6ec054aa42afb0011037d
Mazayd <garagulya699@gmail.com>
2023-08-22 17:51:15 +0300

    added post services

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const UserServices = require("./services/UserServices");
+const PostServices = require("./services/PostServices");
 
 const userServices = new UserServices();
+const postServices = new PostServices();
 
 async function result() {
-  console.log(await userServices.updateUser("12334", { sex: "мужчина" }));
+  console.log(
+    await postServices.updatePost("64e4bbfdb53ed1b30dab81a2", "12334", {
+      content: "da",
+      hashtags: ["magic", "hui"],
+    })
+  );
 }
 
 result();
---
+++ services/PostServices.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+const store = require("../store");
+
+class PostServices {
+  async newPost(telegram_id, body) {
+    try {
+      const { data } = await axios.post(
+        `${store.baseUrl}/post/${telegram_id}`,
+        body
+      );
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async getPostById(id) {
+    try {
+      const { data } = await axios.get(`${store.baseUrl}/post/${id}`);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async getPostsByTags(body) {
+    try {
+      console.log(body);
+      const { data } = await axios({
+        method: "get",
+        url: `${store.baseUrl}/post/`,
+        data: body,
+      });
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async getPostByUserId(id) {
+    try {
+      const { data } = await axios.get(`${store.baseUrl}/post/user/${id}`);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async updatePost(post_id, telegram_id, body) {
+    try {
+      const { data } = await axios.patch(
+        `${store.baseUrl}/post/${post_id}/${telegram_id}`,
+        body
+      );
+      return data;
+    } catch (err) {
+      console.error();
+    }
+  }
+}
+
+module.exports = PostServices;
